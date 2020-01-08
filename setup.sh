1f01661517a75f6ec5ed64ddcbf9885c4cf1ea5e
Marcus Vogel <vogl91@gmail.com>
2020-01-08 22:34:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-docker build -t filter --build-arg PORT=6660 --build-arg REGEX="[^0123456789abcdefghijklmnopqrstuvwxyz/= ._'']"	--build-arg FLAG="SIG{example_flag}" .
+docker build -t filter --build-arg PORT=6660 --build-arg REGEX="[^0123456789abcdefghijklmnopqrstuvwxyz/= ._']"	--build-arg FLAG="SIG{example_flag}" .
 docker run -P -it filter
